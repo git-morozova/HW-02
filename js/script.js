77c78f928f8158fc74733850ca77204fafa8fa1f
as.morozova <3d.morozova@gmail.com>
2024-01-12 01:32:31 +0300

    add all files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* отображение семантических тегов для IE */
+ document.createElement('nav');
+ document.createElement('article');
+ document.createElement('header');
+ document.createElement('section');
+ document.createElement('footer');
+/* отображение семантических тегов для IE */
